d4fd3207bb3c99302a06d6b0d13ed4c32ba2cdec
Naoki Kimura <n-kimura@fores.jp>
2022-01-20 14:09:34 +0900

    8-1

---
+++ ch8/ch8-2/ch8-2-2/target/debug/deps/ch8_2_2.d
@@ -0,0 +1,5 @@
+C:\Users\n-kimura\Documents\rust-4\ch8\ch8-2\ch8-2-2\target\debug\deps\ch8_2_2.exe: src\main.rs
+
+C:\Users\n-kimura\Documents\rust-4\ch8\ch8-2\ch8-2-2\target\debug\deps\ch8_2_2.d: src\main.rs
+
+src\main.rs:
